b9a0d560ef1fea2075c718b7bcf4dd91e86e6f4b
Matej Krajčovič <elbarto512@gmail.com>
2015-03-27 01:29:10 +0100

    Fix long lines

--- test/test_achievements_by_person.py
+++ test/test_achievements_by_person.py
@@ -4,11 +4,13 @@ from . import BaseTestCase
 class TestAchievementsByPerson(BaseTestCase):
     def test_get_achievements_by_person(self):
         expected = {'achievements': [{'age': 21,
-                                      'description': 'Founded Apple Computer Company with Wozniak'},
+                                      'description': 'Founded Apple Computer '
+                                                     'Company with Wozniak'},
                                      {'age': 50,
                                       'description': 'Stanford speech'}]}
         response = self.client.get('/achievements/Steve_Jobs/')
-        self.assertItemsEqual(response.json['achievements'], expected['achievements'])
+        self.assertItemsEqual(response.json['achievements'],
+                              expected['achievements'])
         self.assertTrue(response.status_code == 200)
 
     def test_not_found(self):
