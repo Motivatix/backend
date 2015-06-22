f217d23869dd07106894f68e40f4ee182b6f1749
Peter Polacik <polacik9@uniba.sk>
2015-06-22 22:16:38 +0200

    janitoring: refactored database for easy access
     - previously, database and models couldn't be accessed from outside the
       Flask app itself, making it really hard to add objects to database
       from outside
     - changed an app initialization a little
     - this is required for automatic insertion of objects generated from
       outside data to database
    
    Signed-off-by: Peter Polacik <polacik9@uniba.sk>

--- motivatix/__init__.py
+++ motivatix/__init__.py
@@ -2,16 +2,15 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
-db = SQLAlchemy()
 app = None
+db = None
 
 
 def create_app(config_name):
-    global app
+    global app, db
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    with app.app_context():
-        db.init_app(app)
+    db = SQLAlchemy(app)
 
     from models import Person, Achievement
     from views import *
